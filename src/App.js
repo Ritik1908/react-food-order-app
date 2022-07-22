3b70b8a204635e51d2f66e573762887d2e38ef45
Ritik Verma <ritikverma8100775754@gmail.com>
2022-07-22 21:43:13 +0530

    model-made-responsive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+
+  const [cartIsShown, setChartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setChartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setChartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header></Header>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals />
       </main>
